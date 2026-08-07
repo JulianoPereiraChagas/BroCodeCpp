caa42575435e2b24f758efd73e28890fbac96538
Juliano Pereira Chagas <chagas.julianopereira@gmail.com>
2026-08-07 16:43:11 -0300

    arquivos

---
+++ Aula06/helloworld.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+
+// arithmetic operations are performed using operators like +, -, *, /, and %
+
+    int students = 20;
+
+   // students = students + 1;
+    // students+=1;
+    // students++; // increment operator
+
+    //students = students - 1;
+    //students-=1;
+    //students--; // decrement operator
+
+    //students = students * 2;
+    //students*=2;
+    
+    //students = students / 3;
+    //students/=3;
+
+    int remainder = students % 3; // remainder operator
+
+    std::cout << remainder;;
+
+    return 0;
+}
---
+++ Aula06/helloworld02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() 
+{
+
+    //parentesis
+    //multiplication & division
+    //addition & subtraction
+
+    int students = 6 - (5 + 4) * 3 / 2; //it must be 7
+
+    std::cout << students;
+
+
+    return 0;
+}
