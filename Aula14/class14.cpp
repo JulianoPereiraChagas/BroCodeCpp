8ab32ba759e63ae7bf26e2123b10f4d26691cba7
Juliano Pereira Chagas <chagas.julianopereira@gmail.com>
2026-08-12 16:27:59 -0300

    arquivos

--- Aula14/class14.cpp
+++ Aula14/class14.cpp
@@ -11,10 +11,16 @@ else{
     std::cout << "You haven't passed!";
 }*/
 
-grade >= 60 ? std::cout << "You pass!"
-else{
-    std::cout << "You fail!"
-}
+// grade >= 60 ? std::cout << "You pass!" : std::cout << "You fail!";
+
+/* int number = 9;
+
+number % 2 == 1 ? std::cout << "ODD" : std::cout << "EVEN"; */
+
+bool hungry = false;
+
+// hungry ? std::cout << "You are hungry" : std::cout << "You are full";
+
 
 
     return 0;
