67af6a8c25ad4db9018794309d99a015b986c96d
Juliano Pereira Chagas <chagas.julianopereira@gmail.com>
2026-08-13 15:13:59 -0300

    arquivos

--- Aula16/hello.cpp
+++ Aula16/hello.cpp
@@ -5,19 +5,30 @@ int main() {
     double temp;
     char unit;
 
-    std::cout << "******* Temperature conversion *********";
-    std::cout << "F = Fahrenheit";
-    std::cout << "C = Celcius";
+    std::cout << "******* Temperature conversion *********" << std::endl;
+    std::cout << "F = Fahrenheit" << std::endl;
+    std::cout << "C = Celsius" << std::endl;
     std::cout << "What unit would you like to convert to?: ";
     std::cin >> unit;
 
     if (unit == 'F' || unit == 'f' ){
-        std::cout << "Enter the temperature in Celcius: ";
+        std::cout << "Enter the temperature in Celsius: ";
         std::cin >> temp;
 
         temp = (1.8 * temp) + 32.0;
 
-        std::cout << "Temperature is: " << temp << "F/n";
+        std::cout << "Temperature is: " << temp << " F\n";
+    }
+    else if (unit == 'C' || unit == 'c'){
+        std::cout << "Enter the temperature in Fahrenheit: ";
+        std::cin >> temp;
+
+        temp = (temp - 32) / 1.8;
+        std::cout << "Temperature is: " << temp << " C\n";
+
+    }
+    else{
+        std::cout << "Please, enter only C or F\n";
     }
 
     std::cout<< "*******************************************";
