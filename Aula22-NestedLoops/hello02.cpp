dd5d00449384bc8495d09682031c36b78d2048b2
Juliano Pereira Chagas <chagas.julianopereira@gmail.com>
2026-08-15 13:40:56 -0300

    arquivos

--- Aula22-NestedLoops/hello02.cpp
+++ Aula22-NestedLoops/hello02.cpp
@@ -6,14 +6,21 @@ int main() {
     int columns;
     char symbol;
 
-    std::cout << "How many rows?";
+    std::cout << "How many rows? ";
+    std::cin >> rows;
 
-    for(int i = 1; i <= 3; i++)
-    for(int j = 1; j <= 10; j++){
-        std::cout << j << ' ';
+    std::cout << "How many columns? ";
+    std::cin >> columns;
+
+    std::cout << "Enter a symbol ";
+    std::cin >> symbol;
+
+    for(int i = 1; i <= rows; i++){
+        for(int j = 1; j <= columns; j++){
+            std::cout << symbol << ' ';
+        }
+        std::cout << '\n';
     }
-
-    std::cout << '\n';
 
     return 0;
 }
