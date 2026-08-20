b4074e7da549757d019c9054947dfc9e294f4692
Juliano Pereira Chagas <chagas.julianopereira@gmail.com>
2026-08-20 17:01:03 -0300

    arquivos

--- Aula25-JogoDeAdivinharNumerosAleatorios/hello.cpp
+++ Aula25-JogoDeAdivinharNumerosAleatorios/hello.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main() {
 
     int num;
     int guess;
-    int tries;
+    int tries = 0;
 
-    srand(time(null));
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
     num = (rand() % 100) + 1;
-    std:: cout << "********** NUMBER GUESSING GAME **********";
+    std::cout << "********** NUMBER GUESSING GAME **********\n";
 
     do{
-        std::cout << "Enter a guess between (1-100)";
+        std::cout << "Enter a guess between (1-100): ";
         std::cin >> guess;
-    }
-    tries++
+        tries++;
 
-    if guess()
+        if (guess > num){
+            std::cout << "Too high!\n";
+        }
+        else if(guess < num){
+            std::cout << "Too low!\n";
+        }
+        else{
+            std::cout << "CORRECT! # of tries: " << tries << '\n';
+        }
+
+    } while (guess != num);
 
     return 0;
 }
