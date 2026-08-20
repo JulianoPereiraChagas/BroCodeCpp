0f959cc72c821c919658fb891e150e21358cd9d6
Juliano Pereira Chagas <chagas.julianopereira@gmail.com>
2026-08-20 16:55:26 -0300

    arquivos

---
+++ Aula25-JogoDeAdivinharNumerosAleatorios/hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    int num;
+    int guess;
+    int tries;
+
+    srand(time(null));
+    num = (rand() % 100) + 1;
+    std:: cout << "********** NUMBER GUESSING GAME **********";
+
+    do{
+        std::cout << "Enter a guess between (1-100)";
+        std::cin >> guess;
+    }
+    tries++
+
+    if guess()
+
+    return 0;
+}
